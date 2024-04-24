7e1f88b87696278b207e997d82125fb8dc33b3a6
Trần Quân <quanTND9707@gmail.com>
2024-04-24 18:29:47 +0700

    Update change password

---
+++ University Magazine Contribution System/asp/asp/COMP1640-BE/Controllers/SystemPController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogicLayer.Services.SystemService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP1640_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemPController : ControllerBase
+    {
+        private readonly ISystemPServices _systempServices;
+
+        public SystemPController(ISystemPServices systemServices)
+        {
+            _systempServices = systemServices;
+        }
+
+        [HttpGet("Get Submit Date")]
+        public async Task<ActionResult> Get_SubmitDate(string name)
+        {
+            var systemP = await _systempServices.get_submitDate(name);
+            return Ok(systemP);
+        }
+
+        [HttpGet("Get Complete Date")]
+        public async Task<ActionResult> Get_CompleteDate(string name)
+        {
+            var systemP = await _systempServices.get_completeDate(name);
+            return Ok(systemP);
+        }
+
+        [HttpGet("Dashboard")]
+        public async Task<IActionResult> Dashboard()
+        {
+            var data = await _systempServices.Dashboard();
+
+            return Ok(data);
+        }
+    }
+}
